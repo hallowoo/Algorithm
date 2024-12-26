15bb777a0f65987ecc13b2d8f292eed60011ba34
hallowoo <164675169+hallowoo@users.noreply.github.com>
2024-12-26 10:46:04 +0900

    [level 0] Title: 두 수의 차, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    const int a = -50000;
+    const int b = 50000;
+    if(a <= num1 <= b, a <= num2 <= b){answer = num1 - num2;}
+    return answer;
+}
