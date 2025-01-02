4cf788ed3c2c24d6485e8993fe1bded6de79b009
hallowoo <164675169+hallowoo@users.noreply.github.com>
2025-01-02 11:58:18 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if(s.size() == 4){}
+    else if (s.size() == 6){}
+    else {return false;}
+
+
+    for(char c : s){
+        if(!isdigit(c)){return false ;}
+            else{}        
+    }
+    
+   
+    
+    return answer;
+}
