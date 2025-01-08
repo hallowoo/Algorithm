5132b3a4d8072f7c17d3263f140ae19b271afb9e
hallowoo <164675169+hallowoo@users.noreply.github.com>
2025-01-08 19:06:29 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    string answer = "";
+    vector<string>day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+    vector<int>month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int totDays = 0;
+    for(int i = 0; i < a - 1; i++){
+        totDays += month[i];
+    }
+    totDays = totDays + b;
+    answer = day[totDays%7];
+    return answer;
+}
